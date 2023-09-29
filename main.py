c0b8cda8c1b01681b69e1d3d4060cdb7c162fa9b
John Castro <johhncaastro@gmail.com>
2023-09-29 14:23:30 -0500

    refactored scraper and added df to collect tweets. next removing sleeps and adding waits to mkae program faster.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
@@ -14,7 +16,7 @@ web = 'https://twitter.com/'
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get(web)
 driver.maximize_window()
-time.sleep(5)
+# time.sleep(5)
 
 # login btn
 driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div').click()
@@ -46,16 +48,25 @@ people_btn.click()
 time.sleep(5)
 
 # clicks user and lands on page to begin scraping.
-user_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[1]/div/div/div/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span/span[1]')
+user_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[1]/div/div/div/div/div[2]/div[1]')
 user_btn.click()
 time.sleep(5)
+
+user_data = []
+text_data = []
 
 # scraper
 tweets = driver.find_elements(By.XPATH,"//article[@role='article']")
 
 for tweet in tweets:
-    x_name = tweet.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/section/div/div/div[2]/div/div/article/div/div/div[2]/div[2]/div[1]/div/div[1]/div/div/div[2]/div/div[1]/a/div/span').text
-    print(x_name)
+    x_name = tweet.find_element(By.XPATH, './/span[contains(text(), "@")]').text
+    text = tweet.find_element(By.XPATH, './/div[@lang]').text
+    text = " ".join(text.split())
+    user_data.append(x_name)
+    text_data.append(text)
 
-time.sleep(1000)
 driver.quit()
+
+df_tweets = pd.DataFrame({'user': user_data, 'tweet': text_data})
+df_tweets.to_csv(user+'_tweets.csv', index=False)
+print(df_tweets)
